5c348a6ae1a37fbef67f887de486cc2d06eebb8e
Daniel-Slabaugh <danielslabaugh@gmail.com>
2017-09-16 09:41:11 -0500

    added correct model for sailboat db

--- models.js
+++ models.js
@@ -1,31 +1,47 @@
 const mongoose = require('mongoose');
 
-const blogPostSchema = mongoose.Schema({
-  author: {
-    firstName: String,
-    lastName: String
+const sailboatSchema = mongoose.Schema({
+  owner: {
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
   },
-  title: {type: String, required: true},
-  content: {type: String},
-  created: {type: Date, default: Date.now}
+  address: {
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+  description: String, 
+  year: Number, 
+  condition: String,
+  visible: Boolean, 
+  forSale: Boolean, 
 });
 
 
-blogPostSchema.virtual('authorName').get(function() {
-  return `${this.author.firstName} ${this.author.lastName}`.trim();
+blogPostSchema.virtual('ownerName').get(function() {
+  return `${this.owner.firstName} ${this.owner.lastName}`.trim();
 });
 
-blogPostSchema.methods.apiRepr = function() {
+blogPostSchema.virtual('addressString').get(function() {
+  return `${this.address.street}, ${this.address.city}, ${this.address.state}, ${this.address.zipcode}, ${this.address.country}`;
+});
+
+blogPostSchema.methods.simpleSailboat = function() {
   return {
     id: this._id,
-    author: this.authorName,
-    content: this.content,
-    title: this.title,
-    created: this.created
+    owner: this.ownerName,
+    address: this.addressString,
+    description: this.description, 
+    year: this.year, 
+    condition: this.condition,
+    visible: this.visible, 
+    forSale: this.forSale
   };
 }
 
-const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+const Sailboat = mongoose.model('Sailboat', blogPostSchema);
 //for test purposes
 
-module.exports = {Sailboats};
+module.exports = {Sailboat};
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 
 const {DATABASE_URL, PORT} = require('./config');
-const {Sailboats} = require('./models');
+const {Sailboat} = require('./models');
 
 const app = express();
 

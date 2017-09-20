2d0c8a38b35766bc9963bcde50a3d88da6b5d2ee
Daniel-Slabaugh <danielslabaugh@gmail.com>
2017-09-20 00:00:23 -0500

    added home page

--- public/app.js
+++ public/app.js
@@ -2,10 +2,18 @@
 $(document).ready(function() {
 
   $("#signup-page").hide();
+  $("#home-page").hide();
+
 
   $("#btn-signup").click(function(e) {
     e.preventDefault();
     $("#landing-page").hide();
     $("#signup-page").show();
+  });
+
+  $("#start").submit(function(e) {
+    e.preventDefault();
+    $("#home-page").show();
+    $("#landing-page").hide();    
   });
 });

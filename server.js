d2bfb27b590611b169350cecee094c359cf12a84
Daniel-Slabaugh <danielslabaugh@gmail.com>
2017-10-29 20:08:42 -0500

    removed jwt stuff

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const passport = require('passport');
 const {DATABASE_URL, PORT} = require('./config');
 const {router: sailboatRouter, Sailboats} = require('./sailboats');
 const {router: usersRouter} = require('./users');
-const {router: authRouter, basicStrategy, jwtStrategy} = require('./auth');
 
 
 const app = express();
@@ -24,31 +23,6 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use('/sailboats', sailboatRouter);
 app.use('/users/', usersRouter);
-app.use('/auth/', authRouter);
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-    if (req.method === 'OPTIONS') {
-        return res.send(204);
-    }
-    next();
-});
-app.use(passport.initialize());
-passport.use(basicStrategy);
-passport.use(jwtStrategy);
-
-
-// A protected endpoint which needs a valid JWT to access it
-app.get(
-    '/protected',
-    passport.authenticate('jwt', {session: false}),
-    (req, res) => {
-        return res.json({
-            data: 'rosebud'
-        });
-    }
-);
 
 let server;
 
